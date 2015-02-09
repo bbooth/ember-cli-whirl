de6763ace7359c20a8e3ffa8172dca1cfc8d1059
Bradley Booth <bradley.booth@gmail.com>
2015-02-09 06:00:30 -0700

    Removed comments

--- blueprints/ember-cli-whirl/index.js
+++ blueprints/ember-cli-whirl/index.js
@@ -1,8 +1,5 @@
 module.exports = {
   normalizeEntityName: function() {
-    // allows us to run ember -g ember-cli-bootstrap and not blow up
-    // because ember cli normally expects the format
-    // ember generate <entitiyName> <blueprint>
   },
 
   afterInstall: function () {
